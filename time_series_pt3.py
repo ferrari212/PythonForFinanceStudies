4efbc8ef0c4c3e94b10d3145bd5cc1245f818b39
ferrari212 <40800718+ferrari212@users.noreply.github.com>
2019-05-10 19:44:11 -0300

    Finishing Time Series

--- time_series_pt3.py
+++ time_series_pt3.py
@@ -4,9 +4,15 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# Libraries from time-series correlation
 import statsmodels.api as sm
 from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from pandas.plotting import autocorrelation_plot
+from statsmodels.tsa.arima_model import ARIMA
+from pandas.tseries.offsets import DateOffset
 
 df = pd.read_csv('monthly-milk-production-pounds-production.txt')
 print (df.head())
@@ -57,6 +63,7 @@ def adf_check(time_series):
 
 adf_check(df['Milk in Pounds per Cow'])
 
+# First Difference
 df['First Difference'] = df['Milk in Pounds per Cow'] - df['Milk in Pounds per Cow'].shift(1)
 
 df['First Difference'].plot()
@@ -65,6 +72,7 @@ plt.show()
 print ('\n')
 adf_check(df['First Difference'].dropna())
 
+# Seccond Difference
 df['Seccond Difference'] = df['First Difference'] - df['First Difference'].shift(1)
 
 df['Seccond Difference'].plot()
@@ -72,3 +80,55 @@ plt.show()
 
 print ('\n')
 adf_check(df['Seccond Difference'].dropna())
+
+# Seasonal Difference
+df['Seasonal Difference'] = df['Milk in Pounds per Cow'] - df['Milk in Pounds per Cow'].shift(12)
+
+df['Seasonal Difference'].plot()
+plt.show()
+
+print ('\n')
+adf_check(df['Seasonal Difference'].dropna())
+
+# Seasonal First Difference
+df['Seasonal First Difference'] = df['First Difference'] - df['First Difference'].shift(12)
+
+df['Seasonal First Difference'].plot()
+plt.show()
+
+print ('\n')
+adf_check(df['Seasonal First Difference'].dropna())
+
+fig_first = plot_acf(df['First Difference'].dropna())
+plt.show()
+
+fig_first = plot_acf(df['Seasonal First Difference'].dropna())
+plt.show()
+
+autocorrelation_plot(df['Seasonal First Difference'].dropna())
+plt.show()
+
+# Using ARIMA models with seasona arima SARIMAX
+# help(ARIMA)
+model = sm.tsa.statespace.SARIMAX(df['Milk in Pounds per Cow'], order=(0,1,0), seasonal_order=(1,1,1,12))
+results = model.fit()
+print(results.summary())
+results.resid.plot()
+plt.show()
+
+results.resid.plot(kind='kde')
+plt.show()
+
+# does not show future forecast
+df['forecast'] = results.predict(start=150, end=168)
+df[['Milk in Pounds per Cow','forecast']].plot()
+plt.show()
+
+# This will predict future dates
+future_dates = [df.index[-1] + DateOffset(months=x) for x in range(1,24)]
+print(future_dates)
+future_df = pd.DataFrame(index=future_dates, columns=df.columns)
+final_df = pd.concat([df, future_df])
+final_df['forecast'] = results.predict(start=168, end=192)
+final_df[['Milk in Pounds per Cow','forecast']].plot()
+plt.show()
